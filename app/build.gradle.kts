23c4237048fa20d7461083699ee190401a75bd4d
Aphiwe Mhotwana <127925737+ST10085670@users.noreply.github.com>
2025-05-05 01:07:40 +0200

    my app works

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.androidx.annotation)
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.firebase.storage.ktx)
     // implementation(project(":app"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
